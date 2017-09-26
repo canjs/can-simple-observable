42c7d83b7e04e029eb2c46437ecd582be4fad0fe
Kevin Phillips <kevin@bitovi.com>
2017-09-26 08:29:21 -0500

    1.0.0

---
+++ dist/cjs/can-simple-observable.js
@@ -0,0 +1,41 @@
+/*can-simple-observable@0.0.0#can-simple-observable*/
+var canReflect = require('can-reflect');
+var canBatch = require('can-event/batch/batch');
+var Observation = require('can-observation');
+var CID = require('can-cid');
+module.exports = function reflectiveValue(initialValue) {
+    var value = initialValue;
+    var handlers = [];
+    var fn = function (newValue) {
+        if (arguments.length) {
+            value = newValue;
+            handlers.forEach(function (handler) {
+                canBatch.queue([
+                    handler,
+                    fn,
+                    [newValue]
+                ]);
+            }, this);
+        } else {
+            Observation.add(fn);
+            return value;
+        }
+    };
+    CID(fn);
+    canReflect.assignSymbols(fn, {
+        'can.onValue': function (handler) {
+            handlers.push(handler);
+        },
+        'can.offValue': function (handler) {
+            var index = handlers.indexOf(handler);
+            handlers.splice(index, 1);
+        },
+        'can.setValue': function (newValue) {
+            return fn(newValue);
+        },
+        'can.getValue': function () {
+            return fn();
+        }
+    });
+    return fn;
+};
