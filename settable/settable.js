1f05c74a690b7a383d3bdf06273bfeda6f3d0926
Justin Meyer <justinbmeyer@gmail.com>
2017-10-06 14:57:48 -0500

    tabs

--- settable/settable.js
+++ settable/settable.js
@@ -7,65 +7,65 @@ var queues = require("can-queues");
 
 
 function SettableObservable(fn, context, initialValue) {
-    this.handlers = new KeyTree([Object, Array],{
-        onFirst: this.setup.bind(this),
-        onEmpty: this.teardown.bind(this)
-    });
-    this.lastSetValue = new SimpleObservable(initialValue);
-    this.observation = new Observation(function(){
-        return fn.call(context, this.lastSetValue.get());
-    }, this);
-    this.handler = this.handler.bind(this);
+	this.handlers = new KeyTree([Object, Array], {
+		onFirst: this.setup.bind(this),
+		onEmpty: this.teardown.bind(this)
+	});
+	this.lastSetValue = new SimpleObservable(initialValue);
+	this.observation = new Observation(function() {
+		return fn.call(context, this.lastSetValue.get());
+	}, this);
+	this.handler = this.handler.bind(this);
 }
 var peek = ObservationRecorder.ignore(canReflect.getValue.bind(canReflect));
 
 
 SettableObservable.prototype = {
-    handler: function(newVal) {
-        var old = this.value;
+	handler: function(newVal) {
+		var old = this.value;
 		this.value = newVal;
 		// adds callback handlers to be called w/i their respective queue.
-		queues.enqueueByQueue(this.handlers.getNode([]), this, [newVal, old], function(){
+		queues.enqueueByQueue(this.handlers.getNode([]), this, [newVal, old], function() {
 			return {};
 		});
-    },
-    setup: function(){
-        this.bound = true;
-        canReflect.onValue( this.observation, this.handler, "notify");
-        this.value = peek( this.observation );
-    },
-    teardown: function(){
-        this.bound = false;
-        canReflect.offValue( this.observation, this.handler, "notify");
-    },
-    set: function(newVal){
-        if( newVal !== this.lastSetValue.get() ) {
-            this.lastSetValue.set(newVal);
-        }
-    },
-    get: function(){
-        if(ObservationRecorder.isRecording()) {
-            ObservationRecorder.add(this);
-            if(!this.bound) {
-                Observation.temporarilyBind(this);
-            }
-        }
-
-        if(this.bound === true) {
-            return this.value;
-        } else {
-            return this.observation.get();
-        }
-    },
-    on: function(handler, queue){
-		this.handlers.add([queue|| "mutate", handler]);
 	},
-	off: function(handler, queue){
-		this.handlers.delete([queue|| "mutate", handler]);
+	setup: function() {
+		this.bound = true;
+		canReflect.onValue(this.observation, this.handler, "notify");
+		this.value = peek(this.observation);
+	},
+	teardown: function() {
+		this.bound = false;
+		canReflect.offValue(this.observation, this.handler, "notify");
+	},
+	set: function(newVal) {
+		if (newVal !== this.lastSetValue.get()) {
+			this.lastSetValue.set(newVal);
+		}
+	},
+	get: function() {
+		if (ObservationRecorder.isRecording()) {
+			ObservationRecorder.add(this);
+			if (!this.bound) {
+				Observation.temporarilyBind(this);
+			}
+		}
+
+		if (this.bound === true) {
+			return this.value;
+		} else {
+			return this.observation.get();
+		}
+	},
+	on: function(handler, queue) {
+		this.handlers.add([queue || "mutate", handler]);
+	},
+	off: function(handler, queue) {
+		this.handlers.delete([queue || "mutate", handler]);
 	}
 };
 
-canReflect.assignSymbols(SettableObservable.prototype,{
+canReflect.assignSymbols(SettableObservable.prototype, {
 	"can.getValue": SettableObservable.prototype.get,
 	"can.setValue": SettableObservable.prototype.set,
 	"can.onValue": SettableObservable.prototype.on,
